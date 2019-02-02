c0b90dab237b040ee284d7f2eeef4cb6a73f7f1d
Zsolti37 <petozsolt33@gmail.com>
2019-02-02 12:23:26 +0100

    clean code, I'm not writing out the results. Just the end.

--- prime_test.py
+++ prime_test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-from inpcheck import *
+import inpcheck
 import time
 
 
 print("#"*80)
 print("Meddíg keresse a prímszámokat")
-b = integer_check()
+b = inpcheck.integer_check()
 print("#"*80)
 
 primszamok = []
@@ -19,50 +19,51 @@ while len(primszamok) != b:
         # check for factors
         for i in range(2, num):
             if (num % i) == 0:
-                print(num, "Ez nem prím szám.")
-                print(i, "*", num // i, "=", num)
+#                print(num, "Ez nem prím szám.")
+#                print(i, "*", num // i, "=", num)
                 num += 1
                 break
         else:
-            print(num, "Ez prímszám. Hurrá!!!")
+#            print(num, "Ez prímszám. Hurrá!!!")
             primszamok.append(num)
             num += 1
     # if input number is less than
     # or equal to 1, it is not prime
     else:
-        print(num, "Ez nem prím szám.")
+#        print(num, "Ez nem prím szám.")
         num += 1
 
-print("#"*80)
-print("Ezeket a primszámokat találtad: ", sorted(primszamok))
+print("")
+print("Ezeket a primszámokat találtam: ", sorted(primszamok))
 print("Ez az első", len(primszamok), "db prímszám.")
-print("#"*80)
+print("")
 #
 x = 0
 for i in primszamok:
     x += i
 print("Az első", len(primszamok), "prímszám összege:", x)
-print("#"*80)
+print("")
 
 if x > 1:
     # check for factors
     for i in range(2, x):
         if (x % i) == 0:
-            print(x, "Ez nem prím szám.")
-            print(i, "*", x // i, "=", x)
+#            print(x, "Ez nem prím szám.")
+#            print(i, "*", x // i, "=", x)
             break
     else:
         print(x, "Ez prímszám. Hurrá!!!")
 # if input number is less than
 # or equal to 1, it is not prime
 else:
-    print(x, "Ez nem prím szám.")
+    pass
+#    print(x, "Ez nem prím szám.")
 
 end = time.time()
 e = end - start
-print("#"*80)
+print("")
 print("A program %.2f" % e, "másodpecíg futott.")
-print("#"*80)
+print("")
 
 
 
